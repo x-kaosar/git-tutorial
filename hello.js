62abb69edebd2827001dbefd47f7526107c3e19a
kaosar <mdkaosar01k@gmail.com>
2020-09-10 15:36:22 +0600

    Added a new function called revMe

--- hello.js
+++ hello.js
@@ -2,3 +2,12 @@ function myFunction(){
     document.write('Hello World');
 }
 myFunction();
+
+function revMe(x){
+    return x.split("").reverse().join("");
+}
+
+let str = "A B C D";
+let res = revMe(str);
+
+document.write(res);
